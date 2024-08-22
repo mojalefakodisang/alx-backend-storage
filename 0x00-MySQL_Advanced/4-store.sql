25b6056979c8b16e37af69fe7b4256c6f699eaca
Mojalefa Kodsiang <kodisangmojalefa65@gmail.com>
2024-08-22 19:56:25 +0000

    [Update] - Advanced MySQL

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- Creates a trigger to decrease item quantity after an order
+DELIMITER //
+
+CREATE TRIGGER after_insert_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END //
+
+DELIMITER ;
